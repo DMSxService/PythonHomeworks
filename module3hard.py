f47b4e34c0945f861d2f8a8c578af5e406f23198
Константин <konst1973@yandex.ru>
2024-07-08 08:43:23 +0300

    Reload

--- module3hard.py
+++ module3hard.py
@@ -2,27 +2,26 @@ data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hel
 counter = 0
 def calculate_structure_sum (data_structure):
     global counter
-    if len(data_structure) == 0:
-        return counter
-    elem = data_structure.pop(0)
-    if isinstance(elem, list):
-        data_structure.extend(elem)
-    elif isinstance(elem, str):
-        data_structure.append(len(elem))
-    elif isinstance(elem, int):
-        counter += elem
-    elif isinstance(elem, float):
-        counter += elem
-    elif isinstance(elem, dict):
-        data_structure.extend(elem.keys())
-        data_structure.extend(elem.values())
-    elif isinstance(elem, bool):
-        counter += int(elem)
-    elif isinstance(elem, tuple):
-        data_structure.extend(elem)
-    elif isinstance(elem, set):
-        data_structure.extend(elem)
-    calculate_structure_sum(data_structure)
+    if len(data_structure) != 0:
+        elem = data_structure.pop(0)
+        if isinstance(elem, list):
+            data_structure.extend(elem)
+        elif isinstance(elem, str):
+            data_structure.append(len(elem))
+        elif isinstance(elem, int):
+            counter += elem
+        elif isinstance(elem, float):
+            counter += elem
+        elif isinstance(elem, dict):
+            data_structure.extend(elem.keys())
+            data_structure.extend(elem.values())
+        elif isinstance(elem, bool):
+            counter += int(elem)
+        elif isinstance(elem, tuple):
+            data_structure.extend(elem)
+        elif isinstance(elem, set):
+            data_structure.extend(elem)
+        calculate_structure_sum(data_structure)
     return counter
 
 result = calculate_structure_sum (data_structure)

528ae63728782c85f74ebffab6f94f7874ab2064
Константин <konst1973@yandex.ru>
2024-07-08 17:29:49 +0300

    Reload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as divfm
+from true_math import divide as divtm
+result1 = divfm(69, 3)
+result2 = divfm(3, 0)
+result3 = divtm(49, 7)
+result4 = divtm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)

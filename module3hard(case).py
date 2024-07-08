ca2099a370e1ff31175457803d510fdbc36e08ac
Константин <konst1973@yandex.ru>
2024-07-08 07:58:01 +0300

    Reload

---
+++ module3hard(case).py
@@ -0,0 +1,31 @@
+import builtins
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",((), [{(2, 'Urban', ('Urban2', 35))}])]
+counter = 0
+def calculate_structure_sum (data_structure):
+    global counter
+    elem = data_structure.pop(0)
+    el_tp = type(elem)
+    match el_tp:
+        case builtins.list:
+            data_structure.extend(elem)
+        case builtins.str:
+            data_structure.append(len(elem))
+        case builtins.int:
+            counter += elem
+        case builtins.float:
+            counter += elem
+        case builtins.dict:
+            data_structure.extend(elem.keys())
+            data_structure.extend(elem.values())
+        case builtins.bool:
+            counter += int(elem)
+        case builtins.tuple:
+            data_structure.extend(elem)
+        case builtins.set:
+            data_structure.extend(elem)
+    if len(data_structure)>0:
+        calculate_structure_sum(data_structure)
+    return counter
+
+result = calculate_structure_sum (data_structure)
+print(result)
--- module3hard.py
+++ module3hard.py
@@ -1,5 +1,4 @@
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",((), [{(2, 'Urban', ('Urban2', 35))}])]
-elem = 0
 counter = 0
 def calculate_structure_sum (data_structure):
     global counter

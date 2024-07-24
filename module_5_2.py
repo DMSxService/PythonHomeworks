5bd7a370cd266e4b44b041fc8d2e6534c85bcaaf
Константин <konst1973@yandex.ru>
2024-07-24 07:48:47 +0300

    Reload

--- module_5_2.py
+++ module_5_2.py
@@ -2,8 +2,10 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def __len__(self):
         return self.number_of_floors
+
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
@@ -11,14 +13,17 @@ class House:
         if new_floor < 1 or new_floor > self.number_of_floors:
             print('Такого этажа не существует')
         else:
-            for i in range(1,new_floor+1):
+            for i in range(1, new_floor + 1):
                 print(i)
+
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-
+h3 = h2
 # __str__
 print(h1)
 print(h2)
+print(h3)
 
 # __len__
 print(len(h1))

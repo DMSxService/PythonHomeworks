dbcf649b1eccdf461b6901290024eae2d7bf5212
DMSxService <konst1973@yandex.ru>
2024-08-11 06:12:24 +0300

    Reload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:  # if type(a) != int or type(b) != int or type(a) != float or type(b) != float:
+        a + b
+    except:
+        return str(a) + str(b)
+    return "{:.3f}".format(a+b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))

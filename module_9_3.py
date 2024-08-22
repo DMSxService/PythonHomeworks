10827bd45fe31945a23dbfed57059f15c8c2a3a2
DMSxService <konst1973@yandex.ru>
2024-08-22 11:08:20 +0300

    Reload

--- module_9_3.py
+++ module_9_3.py
@@ -1,10 +1,8 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = [len(list(zip(first, second))[i][0]) - len(list(zip(first, second))[i][1])
-                for i in range(0, min(len(second), len(first)))
-                if len(list(zip(first, second))[i][0]) - len(list(zip(first, second))[i][1])]
-second_result = [len(first[i]) == len(second[i]) for i in range(0, min(len(second), len(first)))]
+first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
 print(list(second_result))

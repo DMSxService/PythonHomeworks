b820f13d015caa09ed5050d81c81faeb0d8c3865
DMSxService <konst1973@yandex.ru>
2024-09-11 14:19:42 +0300

    Reload

---
+++ module_10_4.py
@@ -0,0 +1,50 @@
+import queue
+from threading import Thread
+from random import randint
+from time import sleep
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+
+class Guest(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        waiting = randint(3, 30)
+        for i in range(waiting):
+            sleep(1)
+
+
+class Cafe:
+    def __init__(self, *tables):
+        self.tables = tables
+        self.queue = queue.Queue
+
+    def guest_arrival(self, *guests):
+        pass
+
+    def discuss_guests(self):
+        pass
+
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+    'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+    'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+cafe.discuss_guests()

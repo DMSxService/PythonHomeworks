124291815dfcb656dbcc1c926669c25181b2be8b
DMSxService <konst1973@yandex.ru>
2024-09-23 08:19:17 +0300

    No comment

--- module_10_5.py
+++ module_10_5.py
@@ -1,5 +1,36 @@
+import multiprocessing
+import sys
+from datetime import datetime
 
 
 def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as strings:
+        string = strings.readline()
+        while len(string) > 0:
+            all_data.append(string)
+            string = strings.readline()
 
-    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+# Линейный вызов
+time_start = datetime.now()
+for i in filenames:
+    read_info(i)
+time_end = datetime.now()
+time_res = time_end - time_start
+print(f'{time_res} линейный')
+
+# Многопроцессный
+if __name__ == '__main__':
+    time_start = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, filenames)
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print(f'{time_res} многопроцессный')
+
+
+# 0:00:09.376185 линейный
+# 0:00:04.227996 многопроцессный
